4b2100aa0ecf6ba0f3369ac63276987aa63b6fdd
ivan muliarchyk <ya-gans@mail.ru>
2017-10-25 13:47:41 +0300

    Add card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+	MatToolbarModule,
+	MatInputModule,
+	MatCardModule,
+	MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
